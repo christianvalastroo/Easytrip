5bdf39fdc9f8b808c953d127643c0d3bb46edd26
Christian Valastro <christianvalastro@icloud.com>
2026-06-26 12:51:33 +0200

    Add user login endpoint

--- Backend/modules/users/users.controller.js
+++ Backend/modules/users/users.controller.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const mongoose = require("mongoose")
 const User = require("./users.schema")
 const BadRequestException = require("../../exceptions/BadRequestException")
@@ -58,7 +59,45 @@ const getUserById = async (req, res, next) => {
     }
 }
 
+const loginUser = async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            throw new BadRequestException("Invalid email or password")
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordValid) {
+            throw new BadRequestException("Invalid email or password")
+        }
+
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "1h" }
+        )
+
+        res.status(200).json({
+            message: "Login successful",
+            user: {
+                id: user._id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email
+            },
+            token
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
 module.exports = {
     registerUser,
+    loginUser,
     getUserById
 }
--- Backend/modules/users/users.routes.js
+++ Backend/modules/users/users.routes.js
@@ -1,10 +1,11 @@
 const express = require("express")
-const { registerUser, getUserById } = require("./users.controller")
+const { registerUser, loginUser, getUserById } = require("./users.controller")
 const { validateRegister } = require("../../middlewares/users.middlewares")
 
 const router = express.Router()
 
 router.post("/register", validateRegister, registerUser)
+router.post("/login", loginUser)
 router.get("/:id", getUserById)
 
 module.exports = router
