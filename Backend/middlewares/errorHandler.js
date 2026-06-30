ddb277973055998f7ca6ac5ce452b4f97bc8a2c0
Christian Valastro <christianvalastro@icloud.com>
2026-06-30 22:13:03 +0200

    Handle Mongoose errors

--- Backend/middlewares/errorHandler.js
+++ Backend/middlewares/errorHandler.js
@@ -1,4 +1,16 @@
 const errorHandler = (err, req, res, next) => {
+    if (err.name === "ValidationError") {
+        return res.status(400).json({
+            message: err.message
+        })
+    }
+
+    if (err.name === "CastError") {
+        return res.status(400).json({
+            message: "Invalid id"
+        })
+    }
+
     const statusCode = err.statusCode || 500
 
     res.status(statusCode).json({
